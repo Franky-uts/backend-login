06ff44b95f630620a0b30400bc5a6f2fbde2e8bc
Franky-uts <107886319+Franky-uts@users.noreply.github.com>
2023-01-17 13:24:54 -0600

    Zy

--- models/User.js
+++ models/User.js
@@ -4,25 +4,25 @@ const userSchema = mongoose.Schema({
     name: {
         type:String,
         require: true, 
-        min: 6,
+        min: 3,
         max: 255
     },
     lastname: {
         type:String,
         require: true, 
-        min: 6,
+        min: 3,
         max: 255
     },
     email:{
         type:String,
         require: true, 
-        min: 6,
+        min: 3,
         max: 255
     },
     password:{
         type:String,
         require: true, 
-        minLength: 6,
+        minLength: 3,
     },
     date:{
         type:Date,
